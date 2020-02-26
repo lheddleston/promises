e2564b5e7a86d91e5fedaae5c3973edecc9452fa
Laura <laura.heddleston@gmail.com>
2020-02-26 09:05:06 -0800

    Callback Review + Promises Exercises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,13 +9,30 @@ var request = require('request');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = function (filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (error, file) => {
+      if (error) {
+        reject(error);
+      } else {
+        var firstLine = file.split('\n').shift();
+        resolve(firstLine);
+      }
+    })
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
-  // TODO
+var getStatusCodeAsync = function (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, statusCode) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response.statusCode);
+      }
+    })
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
